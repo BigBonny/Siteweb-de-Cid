9747c596d3c2ff0a5f13497cf06b6e976c9c5542
BigBonny <koukouloukil@gmail.com>
2024-03-29 02:32:30 +0100

    intial

--- app/(dashboard)/u/[username]/_components/navbar/logo.tsx
+++ app/(dashboard)/u/[username]/_components/navbar/logo.tsx
@@ -29,7 +29,7 @@ export const Logo = () => {
             SID
           </p>
           <p className="text-xs text-muted-foreground">
-            Let's play!
+            Let&apos;s play!
           </p>
         </div>
       </div>
